f185e18f6f993d3dcc5fc4aa6f6de22b2d08894e
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-08-11 05:47:14 -0400

    Controllers: Se agrega controlador para los 'posts'

---
+++ app/controllers/postController.mjs
@@ -0,0 +1,20 @@
+import models from '../../db/models/index.js';
+
+const { Post } = models;
+
+async function index(req, res) {
+  console.log('Se han solicitado todos los posts');
+  res.status(200);
+};
+
+async function create(req, res) {
+  console.log('Se ha solicitado crear un post');
+  res.status(200);
+};
+
+async function destroy(req, res) {
+  console.log('Se ha solicitado borrar el post ' + req.params.id);
+  res.status(200);
+};
+
+export { index, create, destroy };
