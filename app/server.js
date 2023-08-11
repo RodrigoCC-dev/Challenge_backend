1c76c9b18b7ea954737c93bd2e9fa36d57107b26
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-08-11 04:21:46 -0400

    Config: Se agregan rutas de la app a través de 'Router' de 'express'

--- app/server.js
+++ app/server.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-//import router from './routes'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import router from './routes/index.js';
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(cors({ origin: process.env.CORS_ORIGINS }))
+app.use(cors({ origin: process.env.CORS_ORIGINS }));
 
-//app.use('/', router)
+app.use('/', router);
 
-app.listen(port)
-console.log('APP escuchando en el puerto ' + port)
+app.listen(port);
+console.log('APP escuchando en el puerto ' + port);
