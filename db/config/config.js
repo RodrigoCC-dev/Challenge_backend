0a6f71f023605ca366500738441302d71a5a1d91
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-08-11 04:49:24 -0400

    Config: Se configura el uso de 'sequelize' en la aplicación

---
+++ db/config/config.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import 'dotenv/config';
+
+const config = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: 'challenge_dev',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: 'challenge_test',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: 'challenge_production',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  }
+}
